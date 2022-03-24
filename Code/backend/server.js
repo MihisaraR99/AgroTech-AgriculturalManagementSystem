894fedadb2df531b064f5b2d62038fe36b1f8cb1
HP <navodapiumi691>
2022-03-24 08:24:58 +0530

    Create backend routes, models and controllers

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -4,12 +4,17 @@ const logger = require("pino")();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
+
 // Import routes to here
 const storeRoutes = require("./routes/storeRoutes");
+const wholesaleRoutes = require("./routes/wholesaleRoutes");
+const CompanyRequest = require("./routes/Pr_companyRoutes");
 
 const app = express();
 dotenv.config();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
@@ -19,7 +24,7 @@ mongoose.connect(process.env.DB_URL, {
 
 const connection = mongoose.connection;
 connection.once("open", () => {
-  logger.info("Mongodb connected successfully");
+  logger.info(" Mongodb connected successfully");
 });
 
 app.get("/", (req, res) => {
@@ -28,6 +33,8 @@ app.get("/", (req, res) => {
 
 // Implement the routes from here
 app.use("/api/store", storeRoutes);
+app.use("/api/wholesale", wholesaleRoutes);
+app.use("/api/companyRequest", CompanyRequest);
 
 app.listen(PORT, () => {
   logger.info(`Server is running on PORT: ${PORT}`);
