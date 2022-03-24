18685df9b6179d4eb22776b2227974722019d489
dimanthathilakarathna98 <walahanduwathilakarathna@gmail.com>
2022-03-24 08:51:37 +0530

    course server ,coursecontroller,coursemodel,courseroute

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -4,12 +4,17 @@ const logger = require("pino")();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
 // Import routes to here
 const storeRoutes = require("./routes/storeRoutes");
+const courseRoutes=require("./routes/courseRoutes");
 
 const app = express();
 dotenv.config();
 app.use(cors());
+app.use(express.json());
+
+
 
 const PORT = process.env.PORT || 8000;
 
@@ -26,8 +31,11 @@ app.get("/", (req, res) => {
   res.status(200).json({ messsage: "Server is running!" });
 });
 
+
 // Implement the routes from here
 app.use("/api/store", storeRoutes);
+app.use("/api/course",require("./routes/courseRoutes"));
+
 
 app.listen(PORT, () => {
   logger.info(`Server is running on PORT: ${PORT}`);
