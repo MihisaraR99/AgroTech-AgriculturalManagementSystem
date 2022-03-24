a8270635cff75ab16b8699298dda3acd089236a5
Samitha Dilshan <it20607678@my.sliit.lk>
2022-03-24 15:30:56 +0530

    Create event management backend routes

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -3,14 +3,17 @@ const dotenv = require("dotenv");
 const logger = require("pino")();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const eventModel = require('./models/eventsModel')
 
 // Import routes to here
 const storeRoutes = require("./routes/storeRoutes");
+const eventRoutes = require("./routes/eventRoutes");
 
 const app = express();
 dotenv.config();
 app.use(cors());
 
+app.use(express.json());
 const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.DB_URL, {
@@ -28,6 +31,7 @@ app.get("/", (req, res) => {
 
 // Implement the routes from here
 app.use("/api/store", storeRoutes);
+app.use("/api/Addevent",require("./routes/eventRoutes"));
 
 app.listen(PORT, () => {
   logger.info(`Server is running on PORT: ${PORT}`);
