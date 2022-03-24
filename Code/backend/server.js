59c0a28f854f352617219c10b7737e2e31d87960
Lalindu Mihisara <mihisara.r99@hotmail.com>
2022-03-24 08:56:37 +0530

    Create career management backend

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 // Import routes to here
 const storeRoutes = require("./routes/storeRoutes");
 const AdsRoutes = require("./routes/AdsRoutes");
+const CandidateRoutes = require("./routes/CandidateRoutes");
+const ApplyforVacancyRoutes = require("./routes/ApplyforVacancyRoutes");
+const ApplyforGuidanceRoutes = require("./routes/ApplyforGuidanceRoutes");
+const AddVacanciesRoutes = require("./routes/AddVacanciesRoutes");
 
 const app = express();
 dotenv.config();
@@ -32,7 +36,11 @@ app.get("/", (req, res) => {
 
 // Implement the routes from here
 app.use("/api/store", storeRoutes);
+app.use("/api/Candidate",require("./routes/CandidateRoutes"));
 app.use("/api/Ads", require("./routes/AdsRoutes"));
+app.use("/api/Applyvacancies", require("./routes/ApplyforVacancyRoutes"));
+app.use("/api/Applyguidances", require("./routes/ApplyforGuidanceRoutes"));
+app.use("/api/AddVacancies",require("./routes/AddVacanciesRoutes"));
 
 app.listen(PORT, () => {
   logger.info(`Server is running on PORT: ${PORT}`);
