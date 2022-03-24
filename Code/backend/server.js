9827121ff0abf3349b191cf351ebc69ddf065b25
Binusha <it20612696@my.sliit.lk>
2022-03-24 08:51:42 +0530

    Lab report all Backends and some fronends

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const logger = require("pino")();
+const bodyPaser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+ 
 // Import routes to here
 const storeRoutes = require("./routes/storeRoutes");
+
+//IT20612696 Senanayake D.B
+const labRoutes = require('./routes/labRoutes.js');
 
 const app = express();
 dotenv.config();
 app.use(cors());
-
+app.use(bodyPaser.json());
 const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.DB_URL, {
@@ -28,7 +32,9 @@ app.get("/", (req, res) => {
 
 // Implement the routes from here
 app.use("/api/store", storeRoutes);
-
+app.use("/api/lab",labRoutes);
 app.listen(PORT, () => {
   logger.info(`Server is running on PORT: ${PORT}`);
 });
+      //Implement the routes from Lab IT20612696 Senanayake D.B
+      //app.use("/reports",labRoutes);
--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
+import axios from 'axios';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AllServices from "./components/AllServices";
 
+//IT20612696 Senanayake D.B
+import AddReport     from "./components/AddReport";
+import AllReports    from "./components/AllReports";
+import DeleteReport  from "./components/DeleteReport";
+import EditReport    from "./components/EditReport";
+import ReportDetails from "./components/ReportDetails";
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<AllServices />} />
+      <Routes> 
+        <Route path="/rep"      element= {<AllReports/>}    />
+        <Route path="/add"      element= {<AddReport/>}     />
+        <Route path="/del"      element= {<DeleteReport/>}  />
+        <Route path="/edit/:id" element= {<EditReport/>}    />
+        <Route path="/rep/:id"  element= {<ReportDetails/>} />
+        <Route path="/"         element= {<AllServices/>}   />
       </Routes>
       <Footer />
     </BrowserRouter>
