647b6b046fcd7b493dc2915c9a66ad617b816a92
Pasindu Prabhashitha-SLIIT <it20610166@my.sliit.lk>
2022-04-14 23:31:38 +0530

    Add more functionality to register

--- Code/frontend/src/components/Users/Register.js
+++ Code/frontend/src/components/Users/Register.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   const onRegister = async () => {
@@ -15,13 +16,28 @@ const Register = () => {
       password,
     };
 
-    const { res, status } = await axios.post(
-      "http://localhost:8000/api/users/sign-up",
-      user
-    );
+    if (
+      user.full_name.length <= 0 ||
+      user.email.length <= 0 ||
+      user.password.length <= 0
+    ) {
+      setError("All the fields are required to create an account");
+      return;
+    }
 
-    if (status === 201) {
-      navigate("/login");
+    var re = /\S+@\S+\.\S+/;
+
+    if (re.test(user.email)) {
+      const { res, status } = await axios.post(
+        "http://localhost:8000/api/users/sign-up",
+        user
+      );
+
+      if (status === 201) {
+        navigate("/login");
+      }
+    } else {
+      setError("Please enter a valid email");
     }
   };
 
@@ -30,6 +46,10 @@ const Register = () => {
       <div className="row  w-100 h-100 d-flex align-items-center justify-content-center">
         <div className="col  w-100 h-100 d-flex align-items-center justify-content-center">
           <form className="jumbotron">
+            {error && (
+              <h5 className="text-center text-danger form-text"> {error} </h5>
+            )}
+
             <div className="mb-3">
               <label for="exampleInputEmail1" className="form-label">
                 Full Name
