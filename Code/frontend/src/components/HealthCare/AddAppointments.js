dce72f722b8b831e42d7dcbc9c84b523119268c1
manil <it20607128@my.sliit.lk>
2022-04-16 13:44:53 +0530

    HealthCare form changes

--- Code/frontend/src/components/HealthCare/AddAppointments.js
+++ Code/frontend/src/components/HealthCare/AddAppointments.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React,{useState} from "react";
 import AddApp from "../HealthCare/Img/gappointment.png";
+import axios from "axios";
 
 import { NavLink } from "react-router-dom";
 
 export default function AddAppointments(){
+
+  /*01--create state*/
+const [CustomerName,setCustomerName] = useState("");
+const [NIC,setNIC] = useState("");
+const [AnimalType,setAnimalType] = useState("");
+const [ContactNo,setContactNo] = useState("");
+const [Address,setAddress] = useState("");
+const [Date,setDate] = useState("");
+const [Time,setTime] = useState("");
+
+
+/*02--add*/
+function sendAppointment(e){
+  e.preventDefault();
+  alert("Going to add new Appointment");
+
+  const newAppointment = {
+    CustomerName,
+    NIC,
+    AnimalType,
+    ContactNo,
+    Address,
+    Date,
+    Time
+  }
+ 
+  /*url*/
+  axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
+    alert("Appointment Added");
+
+   
+  }).catch((err)=>{
+    alert(err)
+    console.log(err);
+  })
+}
+
+
    
       return(
        <>  
@@ -13,14 +52,19 @@ export default function AddAppointments(){
               <div className= "addapp_content">
                 <div className="addapp-form">
                   <h2 className="form-title"> Get an Appointment</h2><br/>
-                  <form className="get-appointment" id="appointment-form">
+                  <form className="get-appointment" id="appointment-form"
+                  onSubmit={sendAppointment}>
+
 
                     <div class="form-group">
                     <label htmlFor="name">
                       <i class="zmdi zmdi-account material-icons-name zmdi-hc-2x"></i>
                     </label>
                     <input type="text" name="name" id="name" autoComplete="off"
-                      placeholder="Your Name" 
+                      placeholder="Your Name" onChange={(e)=>{
+                        setCustomerName(e.target.value);
+                  }}
+           
                     />
                     </div>
                     <br/>
@@ -30,7 +74,10 @@ export default function AddAppointments(){
                       <i class="zmdi zmdi-assignment-account material-icons-name zmdi-hc-2x"></i>
                     </label>
                     <input type="text" name="NIC" id="NIC" autoComplete="off"
-                      placeholder="Your NIC" 
+                      placeholder="Your NIC" onChange={(e)=>{
+                        setNIC(e.target.value);
+                  }}
+          
                     />
                     </div>
                     <br/>
@@ -40,7 +87,10 @@ export default function AddAppointments(){
                       <i class="zmdi zmdi-face material-icons-name zmdi-hc-2x"></i>
                     </label>
                     <input type="text" name="AnimalType" id="AnimalType" autoComplete="off"
-                      placeholder="Breed" 
+                      placeholder="Breed" onChange={(e)=>{
+                        setAnimalType(e.target.value);
+                  }}
+          
                     />
                     </div>
                     <br/>
@@ -50,7 +100,10 @@ export default function AddAppointments(){
                       <i class="zmdi zmdi-phone-in-talk material-icons-name zmdi-hc-2x"></i>
                     </label>
                     <input type="text" name="ContactNo" id="ContactNo" autoComplete="off"
-                      placeholder="Your Mobile No" 
+                      placeholder="Your Mobile No" onChange={(e)=>{
+                        setContactNo(e.target.value);
+                  }}
+          
                     />
                     </div>
                     <br/>
@@ -60,7 +113,10 @@ export default function AddAppointments(){
                       <i class="zmdi zmdi-home material-icons-name zmdi-hc-2x"></i>
                     </label>
                     <input type="text" name="Address" id="Address" autoComplete="off"
-                      placeholder="Your Address" 
+                      placeholder="Your Address" onChange={(e)=>{
+                        setAddress(e.target.value);
+                  }}
+          
                     />
                     </div>
                     <br/>
@@ -70,7 +126,10 @@ export default function AddAppointments(){
                       <i class="zmdi zmdi-calendar material-icons-name zmdi-hc-2x"></i>
                     </label>
                     <input type="text" name="Date" id="Date" autoComplete="off"
-                      placeholder="Date Required" 
+                      placeholder="Date Required" onChange={(e)=>{
+                        setDate(e.target.value);
+                  }}
+          
                     />
                     </div>
                     <br/>
@@ -80,14 +139,17 @@ export default function AddAppointments(){
                       <i class="zmdi zmdi-time material-icons-name zmdi-hc-2x"></i>
                     </label>
                     <input type="text" name="Time" id="Time" autoComplete="off"
-                      placeholder="Time Required" 
+                      placeholder="Time Required" onChange={(e)=>{
+                        setTime(e.target.value);
+                  }}
+          
                     />
                     </div>
                     <br/>
 
                     <div class="mb-3 form-check">
                       <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                        <label class="form-check-label" for="exampleCheck1">Terms and condition apply</label>
+                        <label class="form-check-label" for="exampleCheck1">I accept the Terms of Use & Privacy Policy</label>
                     </div>
 
                     <div className="form-group form-button">
