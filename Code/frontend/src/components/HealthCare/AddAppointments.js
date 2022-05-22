10c626a3267284466971e8b5a12fe1f38ce75540
manil <it20607128@my.sliit.lk>
2022-05-22 11:56:23 +0530

    ui changes

--- Code/frontend/src/components/HealthCare/AddAppointments.js
+++ Code/frontend/src/components/HealthCare/AddAppointments.js
@@ -1,7 +1,8 @@
 import React,{useState} from "react";
-import AddApp from "../HealthCare/Img/gappointment.png";
+import AddApp from "../HealthCare/Img/formappoint.jpg";
 import axios from "axios";
 import {useNavigate, NavLink} from 'react-router-dom';
+import swal from 'sweetalert'
 
 export default function AddAppointments(){
   const navigate = useNavigate();
@@ -31,12 +32,12 @@ function sendAppointment(e){
  
   if (CustomerName == "" || NIC == "" || AnimalType == "" || Address == "" || Date == "" || Time == ""){
     alert("please fill all the required fields!")
-  }else if(ContactNo.length <=9){  alert("please enter valid phone number")
+  }else if(ContactNo.length <=9){  swal({text:"please enter valid phone number", icon:"warning"})
   
   }else{
 /*url*/
 axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
-  alert("Appointment Added");
+  swal({text:"Appointment Added", icon:"success"});
   navigate(`/HInvoice/${CustomerName}/${NIC}/${AnimalType}/${ContactNo}/${Address}/${Date}/${Time}`)
 }).catch((err)=>{
   alert(err)
@@ -50,16 +51,16 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
         
 
 
-            <div className="container mt-5" style={{backgroundColor:"#67f58f", paddingTop:"20px", paddingBottom:"20px", marginBottom:"20px"}}>
+            <div className="container mt-5" style={{backgroundColor:"", paddingTop:"20px", paddingBottom:"20px", marginBottom:"20px"}}>
               <div className="row">
                 <div className="col-sm">
               <div className= "addapp_content">
                 <div className="addapp-form" style={{boxShadow: " 4px 2px 2px rgba(63, 217, 33)" , padding:"40px",width:"450px", backgroundColor:""}}>
-                  <h2 className="form-title"  style={{paddingLeft:"75px"}}> Get an Appointment</h2><br/>
+                  <h2 className="form-title"  style={{paddingLeft:"2px"}}> Get an Appointment</h2><br/>
                   <div className="row">
                   <div className="col col-6">
 
-                  <div className="App-form"  style={{paddingLeft:"190px"}}>
+                  <div className="App-form"  style={{paddingLeft:"100px"}}>
                    
                   <form className="get-appointment" id="appointment-form"
                   onSubmit={sendAppointment}>
@@ -69,7 +70,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     <label htmlFor="name">
                       <i class="zmdi zmdi-account material-icons-name zmdi-hc-2x"></i>
                     </label>
-                    <input  type="text" name="name" id="name" autoComplete="off" style={{backgroundColor:"#98fb98"}}
+                    <input  type="text" name="name" id="name" autoComplete="off" 
                       placeholder="Your Name" onChange={(e)=>{
                         setCustomerName(e.target.value);
                   }} 
@@ -82,7 +83,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     <label htmlFor="NIC">
                       <i class="zmdi zmdi-assignment-account material-icons-name zmdi-hc-2x"></i>
                     </label>
-                    <input type="text" name="NIC" id="NIC" autoComplete="off" style={{backgroundColor:"#98fb98"}}
+                    <input type="text" name="NIC" id="NIC" autoComplete="off"
                       placeholder="Your NIC" onChange={(e)=>{
                         setNIC(e.target.value);
                   }}
@@ -91,11 +92,15 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     </div>
                     <br/>
 
+                  
+                
+
+
                     <div class="form-group d-flex justify-content-around">
                     <label htmlFor="AnimalType">
                       <i class="zmdi zmdi-face material-icons-name zmdi-hc-2x"></i>
                     </label>
-                    <input type="text" name="AnimalType" id="AnimalType" autoComplete="off" style={{backgroundColor:"#98fb98"}}
+                    <input type="text" name="AnimalType" id="AnimalType" autoComplete="off" 
                       placeholder="Breed" onChange={(e)=>{
                         setAnimalType(e.target.value);
                   }}
@@ -108,7 +113,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     <label htmlFor="ContactNo">
                       <i class="zmdi zmdi-phone-in-talk material-icons-name zmdi-hc-2x"></i>
                     </label>
-                    <input type="text" name="ContactNo" id="ContactNo" autoComplete="off" style={{backgroundColor:"#98fb98"}}
+                    <input type="text" name="ContactNo" id="ContactNo" autoComplete="off"
                       placeholder="Your Mobile No" onChange={(e)=>{
                         setContactNo(e.target.value);
                   }}
@@ -121,7 +126,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     <label htmlFor="Address">
                       <i class="zmdi zmdi-home material-icons-name zmdi-hc-2x"></i>
                     </label>
-                    <input type="text" name="Address" id="Address" autoComplete="off" style={{backgroundColor:"#98fb98"}}
+                    <input type="text" name="Address" id="Address" autoComplete="off"
                       placeholder="Your Address" onChange={(e)=>{
                         setAddress(e.target.value);
                   }}
@@ -134,7 +139,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     <label htmlFor="Date">
                       <i class="zmdi zmdi-calendar material-icons-name zmdi-hc-2x"></i>
                     </label>
-                    <input type="text" name="Date" id="Date" autoComplete="off" style={{backgroundColor:"#98fb98"}}
+                    <input type="text" name="Date" id="Date" autoComplete="off"
                       placeholder="Date Required" onChange={(e)=>{
                         setDate(e.target.value);
                   }}
@@ -147,7 +152,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     <label htmlFor="Time">
                       <i class="zmdi zmdi-time material-icons-name zmdi-hc-2x"></i>
                     </label>
-                    <input type="text" name="Time" id="Time" autoComplete="off" style={{backgroundColor:"#98fb98"}}
+                    <input type="text" name="Time" id="Time" autoComplete="off"
                       placeholder="Time Required" onChange={(e)=>{
                         setTime(e.target.value);
                   }}
@@ -158,7 +163,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
 
                     <div className="form-group form-button" style={{paddingLeft:"40px"}}
 >
-                      <button type="submit" class="btn btn-success" >Submit</button>
+                      <button type="submit" class="btn btn-primary" style={{ backgroundColor: "#12af39" }} >Submit</button>
                     </div>
 
                   </form>
@@ -177,7 +182,7 @@ axios.post("http://localhost:8000/api/app/add",newAppointment).then(()=>{
                     
                       <figure>
                       <div className="">
-                        <img src={AddApp} alt="getAppointment pic"/>
+                        <img src={AddApp} alt="getAppointment pic" style={{marginRight:"100px"}}/>
                       </div> 
                       </figure>
                     </div>
